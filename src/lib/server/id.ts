3c16c96cafdc608f64c90f87320643d47c8e934c
nofuruu2 <nfatihul7@gmail.com>
2026-05-20 19:00:33 +0700

    fixing files

--- src/lib/server/id.ts
+++
@@ -1,7 +0,0 @@
-import { encodeBase32LowerCaseNoPadding } from "@oslojs/encoding";
-
-export function generateId(length: number = 15): string {
-	const bytes = new Uint8Array(length);
-	crypto.getRandomValues(bytes);
-	return encodeBase32LowerCaseNoPadding(bytes);
-}
